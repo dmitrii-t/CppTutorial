5a4afc2f9f761687bf01d719347cef7b0c5c039e
Dmitrii Tcimbal <dtcimbal@gmail.com>
2024-11-19 08:48:30 +1100

    Adds an example of inheritance from multiple parent classes

--- Inheritance_Vehicle/src/Main.cpp
+++ Inheritance_Vehicle/src/Main.cpp
@@ -4,49 +4,45 @@ using std::string;
 
 class Vehicle {
 public:
-    int wheels{0};
-    int boot{300};
-    string color = "blue";
+  int wheels{0};
+  int boot{300};
+  string color = "blue";
 
-    void Print() const
-    {
-        std::cout << "This " << color << " vehicle has boot size " << boot << " liters and " << wheels << " wheels!\n";
-    }
+  void Print() const {
+    std::cout << "This " << color << " vehicle has boot size " << boot
+              << " liters and " << wheels << " wheels!\n";
+  }
 };
 
 class Car : public Vehicle {
 public:
-    bool sunroof = false;
+  bool sunroof = false;
 };
 
 class Bicycle : public Vehicle {
 public:
-    bool kickstand = true;
+  bool kickstand = true;
 };
 
-class Truck : public Vehicle
-{
+class Truck : public Vehicle {
 public:
-    bool trailer = true;
+  bool trailer = true;
 };
 
-int main()
-{
-    Car car;
-    car.wheels = 4;
-    car.sunroof = true;
-    car.Print();
-    if(car.sunroof)
-    {
-        std::cout << "And a sunroof!\n";
-    }
+int main() {
+  Car car;
+  car.wheels = 4;
+  car.sunroof = true;
+  car.Print();
+  if (car.sunroof) {
+    std::cout << "And a sunroof!\n";
+  }
 
-    Truck truck;
-    truck.wheels = 6;
-    truck.boot = 3000;
-    if(truck.trailer)
-    {
-        std::cout << "And a trailer!\n";
-    }
-    truck.Print();
+  Truck truck;
+  truck.wheels = 6;
+  truck.boot = 3000;
+  if (truck.trailer) {
+    std::cout << "And a trailer!\n";
+  }
+  truck.Print();
 };
