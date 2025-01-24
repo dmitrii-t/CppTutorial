6c9ae1cdce43e8764700cfae58a0d751fd3cd42d
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-01-25 10:20:14 +1100

    Configures include dir

---
+++ update.ps1
@@ -0,0 +1,7 @@
+﻿# Update CMakeLists.txt in all the projects with the template .templates/CMakeLists.template
+Get-ChildItem -Path "./**/CMakeLists.txt" -Recurse | ForEach-Object {
+    $content = Get-Content ".templates/CMakeLists.template"
+    $folderName = Split-Path -Parent $_.FullName | Split-Path -Leaf
+    $content = $content -replace "project\(.*\)", "project($folderName CXX)"
+    Set-Content $_.FullName $content
+}
