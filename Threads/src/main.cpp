c5b220b5f4864a0622e7e6286903d5dd4a410ac9
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-01-28 07:27:29 +1100

    Updates Threads example

--- Threads/src/main.cpp
+++ Threads/src/main.cpp
@@ -1,23 +1,29 @@
 // Threads Example
 
+#include <iostream>
 #include <thread>
 #include <vector>
 
 using std::vector;
 
-constexpr int                  THREAD_COUNT      = 10;
-constexpr std::chrono::seconds THREAD_SPLEEP_SEC = std::chrono::seconds(5);
+constexpr int                       THREAD_COUNT      = 10;
+constexpr std::chrono::milliseconds THREAD_SPLEEP_SEC = std::chrono::milliseconds(5);
 
 int main() {
     std::vector<std::thread> threads;
 
     for (int i = 0; i < THREAD_COUNT; ++i) {
-        threads.emplace_back(std::thread([]() {
-            while (true) {
-                std::this_thread::sleep_for(THREAD_SPLEEP_SEC);
-            }
+        // emplace_back() instead of push_back() as
+        // emplace_back() uses move semantics to move a thread object instead of copying
+        // it
+        threads.emplace_back(std::thread([i]() {
+            std::this_thread::sleep_for(THREAD_SPLEEP_SEC * i);
+            // std::cout << "worker thread " <<  std::this_thread::get_id() << "\n";
+            std::cout << "worker thread " << i << "\n";
         }));
     }
+
+    std::cout << "main thread" << '\n';
 
     for (auto& thread : threads) {
         thread.join();
