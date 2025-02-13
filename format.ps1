3179f4d3fa49f11bf6dd96cc045af89724fb0339
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-02-14 06:24:21 +1100

    Adds an example on lock guards

---
+++ format.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -Recurse -Include *.c,*.h,*.cpp,*.hpp -Path src | ForEach-Object {
+    & "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-format.exe" -i $_.FullName
+}
